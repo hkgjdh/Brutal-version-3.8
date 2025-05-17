724aac898ff3d9e498b10872b9566371b7e1f753
hkgjdh <eonhemant20@gmail.com>
2025-05-17 16:11:21 +0530

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,4 +1,4 @@
 {
   "version": "0.01",
-  "url": "https://raw.githubusercontent.com/hkgjdh/HK-CHEATS-LOADER-V2/main/libHKCHEATS.so"
+  "url": "https://raw.githubusercontent.com/hkgjdh/Brutal-version-3.8/main/libHKCHEATS.so"
 }
